f65bae8b237b9fee71b15c8dccc078d9443b9df1
Fedde Schaeffer <fedde@thehyve.nl>
2020-06-04 13:58:08 +0200

    Factor filters into a hook

--- src/public/common/FilteringOtTableRF.js
+++ src/public/common/FilteringOtTableRF.js
@@ -5,20 +5,7 @@ import crossfilter from 'crossfilter2';
 import _ from 'lodash';
 
 const FilteringOtTableRF = props => {
-  const [filteredRows, setFilteredRows] = React.useState(props.data);
-  const xf = React.useRef(crossfilter()).current;
-  const dimensions = React.useRef({}).current;
-
-  React.useEffect(
-    () => {
-      xf.add(props.data);
-      setFilteredRows(xf.allFiltered());
-      return () => {
-        xf.remove();
-      };
-    },
-    [props.data, xf]
-  );
+  const [filteredRows, addFilterDropdown] = useFilterDropdowns(props.data);
 
   return (
     <OtTableRF
@@ -29,12 +16,9 @@ const FilteringOtTableRF = props => {
           ? {
               ...column,
               filterable: undefined,
-              renderFilter: filterDropdown(
-                xf,
-                dimensions,
+              renderFilter: addFilterDropdown(
                 column.id,
-                column.dropdownFilterValue,
-                setFilteredRows
+                column.dropdownFilterValue
               ),
             }
           : column
@@ -44,49 +28,61 @@ const FilteringOtTableRF = props => {
   );
 };
 
-const filterDropdown = (
-  xf,
-  dimensions,
-  columnId,
-  dropdownFilterValue,
-  setFilteredRows
-) => {
-  let valueAccessor = dropdownFilterValue || (row => row[columnId]);
-  dimensions[columnId] = dimensions[columnId] || xf.dimension(valueAccessor);
-  const dim = dimensions[columnId];
-  return () => {
-    const handler = selection => {
-      if (selection) {
-        dim.filter(cellValue => {
-          if (_.isArray(cellValue)) {
-            return _.some(
-              cellValue,
-              arrayElement => arrayElement === selection.value
-            );
-          } else {
-            return cellValue === selection.value;
-          }
-        });
-      } else {
-        dim.filterAll();
-      }
+const useFilterDropdowns = rows => {
+  const [filteredRows, setFilteredRows] = React.useState(rows);
+  const xf = React.useRef(crossfilter()).current;
+  const dimensions = React.useRef({}).current;
 
+  React.useEffect(
+    () => {
+      xf.add(rows);
       setFilteredRows(xf.allFiltered());
+      return () => {
+        xf.remove();
+      };
+    },
+    [rows, xf]
+  );
+
+  const addFilterDropdown = (columnId, dropdownFilterValue) => {
+    let valueAccessor = dropdownFilterValue || (row => row[columnId]);
+    dimensions[columnId] = dimensions[columnId] || xf.dimension(valueAccessor);
+    const dim = dimensions[columnId];
+    return () => {
+      const handler = selection => {
+        if (selection) {
+          dim.filter(cellValue => {
+            if (_.isArray(cellValue)) {
+              return _.some(
+                cellValue,
+                arrayElement => arrayElement === selection.value
+              );
+            } else {
+              return cellValue === selection.value;
+            }
+          });
+        } else {
+          dim.filterAll();
+        }
+
+        setFilteredRows(xf.allFiltered());
+      };
+      const cellValues = xf.allFiltered().flatMap(row => {
+        const cellValue = valueAccessor(row);
+        if (_.isArray(cellValue)) {
+          return cellValue;
+        } else {
+          return [cellValue];
+        }
+      });
+      const options = _.sortedUniq(cellValues.sort()).map(value => ({
+        label: value,
+        value: value,
+      }));
+      return <Select isClearable options={options} onChange={handler} />;
     };
-    const cellValues = xf.allFiltered().flatMap(row => {
-      const cellValue = valueAccessor(row);
-      if (_.isArray(cellValue)) {
-        return cellValue;
-      } else {
-        return [cellValue];
-      }
-    });
-    const options = _.sortedUniq(cellValues.sort()).map(value => ({
-      label: value,
-      value: value,
-    }));
-    return <Select isClearable options={options} onChange={handler} />;
   };
+  return [filteredRows, addFilterDropdown];
 };
 
 export default FilteringOtTableRF;
