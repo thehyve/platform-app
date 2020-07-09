f081fc6de14a8e1edc995cedfbd2ebcb118459a4
Fedde Schaeffer <fedde@thehyve.nl>
2020-07-09 16:07:50 +0200

    Use a routing setup that respects our URL scheme
    By using fragment identifier URLs for pages resolved within the
    single-page app, like Gmail does, we can more easily serve the
    React app on a URL beside the Angular app.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
 import { OtUiThemeProvider } from 'ot-ui';
 
