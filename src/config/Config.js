f2be90436e9a8c1fb15708f9273f0901ee298e3d
Roman Hillje <roman.hillje@googlemail.com>
2021-05-11 15:07:52 +0200

    Check availability of configuration variables with "||"
    This is only relevant if we decide to grab configurations from
    environment variables like done here, as opposed to the way it is done
    by EBI in the deployment.

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,30 +1,14 @@
-// function which returns the provided variable if it is not null and not an
-// empty string, otherwise it will return a default value
-function getNonEmptyVarOrDefault(variable, defaultVal) {
-  if (typeof variable !== 'undefined' && variable !== null && variable !== '') {
-    return variable;
-  } else {
-    return defaultVal;
-  }
-}
-
 // Configuration Object
 export const config = {
   googleTagManagerID: window.configGoogleTagManagerID ?? null,
-  urlApi: getNonEmptyVarOrDefault(
-    process.env.REACT_APP_URL_API,
-    'https://api-beta-dot-open-targets-eu-dev.appspot.com/api/v4/graphql'
-  ),
-  urlApiBeta: getNonEmptyVarOrDefault(
-    process.env.REACT_APP_URL_API_BETA,
-    'https://api-beta-dot-open-targets-eu-dev.appspot.com/api/v4/graphql'
-  ),
-  contactEmailLabel: getNonEmptyVarOrDefault(
-    process.env.REACT_APP_CONTACT_EMAIL_LABEL,
-    'helpdesk@opentargets.org'
-  ),
-  contactEmailAddress: getNonEmptyVarOrDefault(
-    process.env.REACT_APP_CONTACT_EMAIL_ADDRESS,
-    'helpdesk@opentargets.org'
-  ),
+  urlApi:
+    process.env.REACT_APP_URL_API ||
+    'https://api.platform.opentargets.org/api/v4/graphql',
+  urlApiBeta:
+    process.env.REACT_APP_URL_API_BETA ||
+    'https://api.platform.opentargets.org/api/v4/graphql',
+  contactEmailLabel:
+    process.env.REACT_APP_CONTACT_EMAIL_LABEL || 'helpdesk@opentargets.org',
+  contactEmailAddress:
+    process.env.REACT_APP_CONTACT_EMAIL_ADDRESS || 'helpdesk@opentargets.org',
 };
